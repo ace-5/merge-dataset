c13caa118bdd6f4c590b26438c11541258b763de
ashmin <ashminniraula@gmail.com>
2022-07-05 01:12:51 +0545

    Write output to csv file

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import enchant
-import json
-
-from rx import merge
+import csv
 district_list = enchant.PyPWL('districts_of_Nepal.txt')
 
 data = [{'Kathmandu':0.8, 'Dhanusa':0.85, 'Kavre palanchowk':0.75},
     {'Kathmandu':0.35, 'Kavrepalanchowk':0.65, 'Dhanusha':0.6}]
 
 merged = {}
-for items in data:
-    for k,v in items.items():
+merged['District'] = []
+for i in range(len(data)):
+    merged[f'KPI_{i+1}'] = []
+    for k,v in data[i].items():
         k = district_list.suggest(k)[0]
-        if k in merged.keys():
-            merged[k].append(v)
+        if k not in merged['District']:
+            merged['District'].append(k)
+            merged[f'KPI_{i+1}'].append(v)
         else:
-            merged[k] = [v]
+            merged[f'KPI_{i+1}'].append(v)
 
-with open('output.json', 'w') as f:
-    f.write(json.dumps(merged))
+with open('output.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(merged.keys())
+    writer.writerows(zip(*merged.values()))
