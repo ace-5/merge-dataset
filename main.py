f4073f42b1965360bcaa9e0a34a9a40f721649ff
ashmin <ashminniraula@gmail.com>
2022-07-04 23:29:08 +0545

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import enchant
+district_list = enchant.PyPWL('districts_of_Nepal.txt')
+
+data = [{'Kathmandu':0.8, 'Dhanusa':0.85, 'Kavre palanchowk':0.75},
+    {'Kathmandu':0.35, 'Kavrepalanchowk':0.65, 'Dhanusha':0.6}]
+
+merged = {}
+for items in data:
+    for k,v in items.items():
+        k = district_list.suggest(k)[0]
+        if k in merged.keys():
+            merged[k].append(v)
+        else:
+            merged[k] = [v]
+
+print(merged)
