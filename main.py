3e03a3359fddbfdf31a18d38a67567f9b30f15ae
ashmin <ashminniraula@gmail.com>
2022-07-05 20:38:49 +0545

    Use pythonic approach

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import enchant
 import csv
+from collections import defaultdict
 district_list = enchant.PyPWL('districts_of_Nepal.txt')
 
 data = [{'Kathmandu':0.8, 'Dhanusa':0.85, 'Kavre palanchowk':0.75},
     {'Kathmandu':0.35, 'Kavrepalanchowk':0.65, 'Dhanusha':0.6}]
 
-merged = {
-    'District': []
-}
+
+merged = defaultdict(list)
 for i in range(len(data)):
-    merged[f'KPI_{i+1}'] = []
     for k,v in data[i].items():
         k = district_list.suggest(k)[0]
-        if k not in merged['District']:
-            merged['District'].append(k)
-        merged[f'KPI_{i+1}'].append(v)
+        merged['District']+=[k]
+        merged[f'KPI_{i+1}']+=[v] 
 
 with open('output.csv', 'w') as f:
     writer = csv.writer(f)
